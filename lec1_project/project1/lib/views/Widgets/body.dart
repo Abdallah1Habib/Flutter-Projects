be7404ce0e9d0a7a20f5de7b8c34cbd8ff17dead
Abdallah1Habib <abdullah.muhammad.200185@btu.edu.eg>
2023-02-24 17:45:40 +0200

    changed the body of column to contain custom  app bar

--- lec1_project/project1/lib/views/Widgets/body.dart
+++ lec1_project/project1/lib/views/Widgets/body.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:project1/views/Widgets/customappbar.dart';
 
 class beboBody extends StatelessWidget {
   const beboBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column();
+    return Column(
+      children: [
+        customAppbar(),
+      ],
+    );
   }
 }
