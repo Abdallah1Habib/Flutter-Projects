535469c692b5cd64d1e5155170da5a09e5c005cf
Abdallah1Habib <abdullah.muhammad.200185@btu.edu.eg>
2023-02-24 17:25:40 +0200

    added views folder and made a beboviews class that returns a scaffold

---
+++ lec1_project/project1/lib/views/bebo_views.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BeboViews extends StatelessWidget {
+  const BeboViews({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
