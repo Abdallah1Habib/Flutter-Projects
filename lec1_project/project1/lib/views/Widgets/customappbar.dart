463c8f5c94dd9845359b9597acc256eefd412b91
Abdallah1Habib <abdullah.muhammad.200185@btu.edu.eg>
2023-02-24 17:45:05 +0200

    created custom appbar file and imported it in body file for use later

---
+++ lec1_project/project1/lib/views/Widgets/customappbar.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class customAppbar extends StatelessWidget {
+  const customAppbar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row();
+  }
+}
