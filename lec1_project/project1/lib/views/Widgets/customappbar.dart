c374eac4fb376b235708c1cb726c3f62c2061d89
Abdallah1Habib <abdullah.muhammad.200185@btu.edu.eg>
2023-02-24 22:11:29 +0200

    moved the custom search icon widget to seperate file

--- lec1_project/project1/lib/views/Widgets/customappbar.dart
+++ lec1_project/project1/lib/views/Widgets/customappbar.dart
@@ -17,24 +17,3 @@ class customAppbar extends StatelessWidget {
     );
   }
 }
-
-class customsearchicon extends StatelessWidget {
-  const customsearchicon({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 45,
-      width: 45,
-      decoration: BoxDecoration(
-        color: Colors.white.withOpacity(.05),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Center(
-          child: Icon(
-        Icons.search,
-        size: 28,
-      )),
-    );
-  }
-}
