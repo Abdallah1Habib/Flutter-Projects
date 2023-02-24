191d8ed9997ad83d48564a3b9af20d0ef06b1115
Abdallah1Habib <abdullah.muhammad.200185@btu.edu.eg>
2023-02-24 22:02:59 +0200

    added the search icon in the app bar class

--- lec1_project/project1/lib/views/Widgets/customappbar.dart
+++ lec1_project/project1/lib/views/Widgets/customappbar.dart
@@ -10,7 +10,8 @@ class customAppbar extends StatelessWidget {
         Text(
           'My Notes',
           style: TextStyle(fontSize: 28),
-        )
+        ),
+        customsearchicon(),
       ],
     );
   }
@@ -24,7 +25,7 @@ class customsearchicon extends StatelessWidget {
     return Container(
       height: 45,
       width: 45,
-      decoration: const BoxDecoration(color: Colors.white),
+      decoration: BoxDecoration(color: Colors.white),
       child: Center(child: Icon(Icons.search)),
     );
   }
