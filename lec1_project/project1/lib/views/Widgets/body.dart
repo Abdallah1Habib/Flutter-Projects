65569e71c0cec0a2904c681e92f59105bebde099
Abdallah1Habib <abdullah.muhammad.200185@btu.edu.eg>
2023-02-24 17:38:44 +0200

    created app body in different file to import in beboViews

---
+++ lec1_project/project1/lib/views/Widgets/body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class beboBody extends StatelessWidget {
+  const beboBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column();
+  }
+}
