92b9441e1c2faa48d30106795af5ef4ed247fc59
Abdallah1Habib <abdullah.muhammad.200185@btu.edu.eg>
2023-03-01 17:26:03 +0200

    moved the note item class to seperate file

--- lec1_project/project1/lib/views/Widgets/body.dart
+++ lec1_project/project1/lib/views/Widgets/body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:project1/views/Widgets/customappbar.dart';
+import 'package:project1/views/Widgets/noteitem.dart';
 
 class beboBody extends StatelessWidget {
   const beboBody({super.key});
@@ -7,59 +8,16 @@ class beboBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 24),
+      padding: const EdgeInsets.symmetric(horizontal: 24),
       child: Column(
-        children: [
+        children: const [
           SizedBox(
             height: 50,
           ),
           customAppbar(),
+          noteItem(),
         ],
       ),
-    );
-  }
-}
-
-class noteItem extends StatelessWidget {
-  const noteItem({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
-      decoration: BoxDecoration(
-          color: const Color(0xffFFCC80),
-          borderRadius: BorderRadius.circular(16)),
-      child: Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
-        ListTile(
-          title: const Text(
-            "Flutter Tips",
-            style: TextStyle(color: Colors.black, fontSize: 26),
-          ),
-          subtitle: Padding(
-            padding: const EdgeInsets.only(top: 16, bottom: 16),
-            child: Text(
-              "Build your career with Abdallah Habib",
-              style:
-                  TextStyle(color: Colors.black.withOpacity(.4), fontSize: 20),
-            ),
-          ),
-          trailing: IconButton(
-            onPressed: () {},
-            icon: Icon(
-              Icons.delete,
-              size: 24,
-            ),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(right: 24),
-          child: Text(
-            "March1 , 2023",
-            style: TextStyle(color: Colors.black.withOpacity(.4), fontSize: 14),
-          ),
-        ),
-      ]),
     );
   }
 }
