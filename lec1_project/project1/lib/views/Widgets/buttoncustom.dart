9f8e1a092655d51ffdd1ef7726e3517ac73bec7b
Abdallah1Habib <abdullah.muhammad.200185@btu.edu.eg>
2023-03-01 19:46:46 +0200

    added on tap function to save data when add button pressed

--- lec1_project/project1/lib/views/Widgets/buttoncustom.dart
+++ lec1_project/project1/lib/views/Widgets/buttoncustom.dart
@@ -2,21 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:project1/views/Widgets/constants.dart';
 
 class custombutton extends StatelessWidget {
-  const custombutton({super.key});
+  const custombutton({super.key, this.onTap});
+  final void Function()? onTap;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      height: 55,
-      decoration: BoxDecoration(
-          color: primarycolor, borderRadius: BorderRadius.circular(8)),
-      child: const Center(
-          child: Text(
-        'Add',
-        style: TextStyle(
-            color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
-      )),
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: 55,
+        decoration: BoxDecoration(
+            color: primarycolor, borderRadius: BorderRadius.circular(8)),
+        child: const Center(
+            child: Text(
+          'Add',
+          style: TextStyle(
+              color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
+        )),
+      ),
     );
   }
 }
