cb459016b1442ef18f5081af7c1f11c3f844385d
Abdallah1Habib <abdullah.muhammad.200185@btu.edu.eg>
2023-03-01 19:00:35 +0200

    created notemodel class to handle database

---
+++ lec1_project/project1/lib/models/notemode.dart
@@ -0,0 +1,11 @@
+class noteModel {
+  final String title = '';
+  final String subtitle = '';
+  final String date = '';
+  final int color = 0;
+  noteMode(
+      {required this.title,
+      required this.subtitle,
+      required this.date,
+      required this.color});
+}
