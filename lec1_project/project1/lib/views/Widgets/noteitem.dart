11856fa761e904f689a9623bd637f45350e5d373
Abdallah1Habib <abdullah.muhammad.200185@btu.edu.eg>
2023-03-01 18:42:15 +0200

    when pressing on note item screen move to edit note view

--- lec1_project/project1/lib/views/Widgets/noteitem.dart
+++ lec1_project/project1/lib/views/Widgets/noteitem.dart
@@ -1,45 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:project1/views/noteview.dart';
 
 class noteItem extends StatelessWidget {
   const noteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
-      decoration: BoxDecoration(
-          color: const Color(0xffFFCC80),
-          borderRadius: BorderRadius.circular(16)),
-      child: Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
-        ListTile(
-          title: const Text(
-            "Flutter Tips",
-            style: TextStyle(color: Colors.black, fontSize: 26),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) {
+          return const editnoteview();
+        }));
+      },
+      child: Container(
+        padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
+        decoration: BoxDecoration(
+            color: const Color(0xffFFCC80),
+            borderRadius: BorderRadius.circular(16)),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
+          ListTile(
+            title: const Text(
+              "Flutter Tips",
+              style: TextStyle(color: Colors.black, fontSize: 26),
+            ),
+            subtitle: Padding(
+              padding: const EdgeInsets.only(top: 16, bottom: 16),
+              child: Text(
+                "Build your career with Abdallah Habib",
+                style: TextStyle(
+                    color: Colors.black.withOpacity(.4), fontSize: 20),
+              ),
+            ),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.delete,
+                size: 24,
+              ),
+            ),
           ),
-          subtitle: Padding(
-            padding: const EdgeInsets.only(top: 16, bottom: 16),
+          Padding(
+            padding: const EdgeInsets.only(right: 24),
             child: Text(
-              "Build your career with Abdallah Habib",
+              "March1 , 2023",
               style:
-                  TextStyle(color: Colors.black.withOpacity(.4), fontSize: 20),
+                  TextStyle(color: Colors.black.withOpacity(.4), fontSize: 14),
             ),
           ),
-          trailing: IconButton(
-            onPressed: () {},
-            icon: Icon(
-              Icons.delete,
-              size: 24,
-            ),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(right: 24),
-          child: Text(
-            "March1 , 2023",
-            style: TextStyle(color: Colors.black.withOpacity(.4), fontSize: 14),
-          ),
-        ),
-      ]),
+        ]),
+      ),
     );
   }
 }
