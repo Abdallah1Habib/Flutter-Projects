a5858fb3b2d61ddea1a0f837f4e744388541690a
Abdallah1Habib <abdullah.muhammad.200185@btu.edu.eg>
2023-03-01 18:24:00 +0200

    made hint and max lines variable to get when building text field

--- lec1_project/project1/lib/views/Widgets/textfield.dart
+++ lec1_project/project1/lib/views/Widgets/textfield.dart
@@ -4,14 +4,17 @@ import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:project1/views/Widgets/constants.dart';
 
 class customtextfield extends StatelessWidget {
-  const customtextfield({super.key});
+  const customtextfield({super.key, required this.hint, this.lines = 1});
 
+  final String hint;
+  final int lines;
   @override
   Widget build(BuildContext context) {
     return TextField(
       cursorColor: primarycolor,
+      maxLines: lines,
       decoration: InputDecoration(
-        hintText: 'Title',
+        hintText: hint,
         hintStyle: const TextStyle(color: primarycolor),
         border: buildborder(),
         enabledBorder: buildborder(),
