150bbb9e82a85a0839460494459245fc91cb9bcf
Abdallah1Habib <abdullah.muhammad.200185@btu.edu.eg>
2023-03-01 18:45:00 +0200

    added the app bar to the edit note view

--- lec1_project/project1/lib/views/Widgets/editviewbody.dart
+++ lec1_project/project1/lib/views/Widgets/editviewbody.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:project1/views/Widgets/customappbar.dart';
 
 class editviewbody extends StatelessWidget {
   const editviewbody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      child: Column(
+        children: [
+          SizedBox(
+            height: 50,
+          ),
+          customAppbar(),
+        ],
+      ),
+    );
   }
 }
