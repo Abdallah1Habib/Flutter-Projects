8e0dbc7901c72073ac609701fe433a2771a6f092
Abdallah1Habib <abdullah.muhammad.200185@btu.edu.eg>
2023-03-01 18:06:11 +0200

    created custom text field class in new file

--- lec1_project/project1/lib/views/Widgets/bottomsheet.dart
+++ lec1_project/project1/lib/views/Widgets/bottomsheet.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:project1/views/Widgets/textfield.dart';
 
 class Addnote extends StatelessWidget {
   const Addnote({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: [customtextfield()],
+    );
   }
 }
