c41ce04115e25279b6d6d6b1b9436ffa939719b3
Abdallah1Habib <abdullah.muhammad.200185@btu.edu.eg>
2023-03-01 17:57:48 +0200

    added padding to the list view from the top

--- lec1_project/project1/lib/views/Widgets/listview.dart
+++ lec1_project/project1/lib/views/Widgets/listview.dart
@@ -7,12 +7,16 @@ class noteslist extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(itemBuilder: (context, index) {
-      // ignore: prefer_const_constructors
-      return Padding(
-        padding: const EdgeInsets.symmetric(vertical: 4),
-        child: const noteItem(),
-      );
-    });
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16),
+      child: ListView.builder(
+          padding: EdgeInsets.zero,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(vertical: 4),
+              child: const noteItem(),
+            );
+          }),
+    );
   }
 }
